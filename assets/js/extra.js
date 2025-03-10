c9be67615a7ccfc0167acc55711ec55ffc16a895
FernandoCelmer <email@fernandocelmer.com>
2025-03-09 23:17:06 -0300

    Deployed 4342842 with MkDocs version: 1.5.2

--- assets/js/extra.js
+++ assets/js/extra.js
@@ -2,13 +2,13 @@ window.onload = function () {
   document.getElementById("show").addEventListener("click", changeBackground);
 
   function changeBackground() {
-      var obj=document.getElementById('top');
-        
-      if(obj.className=='home_bg_01'){
-        obj.className='home_bg_02';
-      }else{
-        obj.className='home_bg_01';
-      }
+    var obj = document.getElementById('top');
+
+    if (obj.className == 'background_initial') {
+      obj.className = 'background_space';
+    } else {
+      obj.className = 'background_initial';
+    }
 
   }
 
@@ -17,19 +17,19 @@ window.onload = function () {
 window.addEventListener("load", initAudioPlayer);
 var audio, playbtn, mutebtn, seek_bar;
 
-function initAudioPlayer(){
+function initAudioPlayer() {
   audio = new Audio();
-  audio.src = "blog/assets/sound/p a l e   b l u e   d o t.mp3";
+  audio.src = "blog/assets/sound/carl-sagan-pale-blue-dot.mp3";
   audio.loop = true;
 
   playbtn = document.getElementById("playpausebtn");
-  playbtn.addEventListener("click",playPause);
+  playbtn.addEventListener("click", playPause);
 
-  function playPause(){
-    if(audio.paused){
-        audio.play();	  
-      } else {
-        audio.pause();
-      }
+  function playPause() {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
   }
 }
